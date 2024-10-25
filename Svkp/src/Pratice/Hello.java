4622b2bf9c0c4bbf0b5d8ddedc96607a1886b6ce
LPT-10 <LPT-10@10.0.0.106>
2024-10-25 15:33:36 +0530

    commit1

---
+++ Svkp/src/Pratice/Hello.java
@@ -0,0 +1,22 @@
+ 
+
+public class ObjectComparsion{
+	public static void main(String[]args ) {
+		
+		Double x=new Double(123.4555);
+		Long y=new Long (9887545);
+			
+			System.out.println(" Objects are not equal,hence returns" + x.euals(y));
+			
+		
+		System.out.println("Objects are equal,hence it returns" +x.equals(123.45555));}
+}
+
+	}
+		
+		
+		
+	} 
+	
+
+}
