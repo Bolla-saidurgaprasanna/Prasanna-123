e1fbcd8dad8f98547d81a1cfc34c0457aa48f9c5
LPT-10 <LPT-10@10.0.0.104>
2024-11-04 16:01:14 +0530

    CommitReturnDemo

--- Svkp/src/Pratice/RelationalOperator.java
+++ Svkp/src/Pratice/RelationalOperator.java
@@ -3,7 +3,14 @@ package Pratice;
 public class RelationalOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=10 ,b=5;
+		
+		System.out.println("a==b "+ (a==b)); //equal to //false
+		System.out.println("a!=b " + (a!=b)); //Not equal to //true
+		System.out.println("a>b " + (a>b)); //Greater than //true
+		System.out.println("a<b " + (a<b)); //less than //false
+		System.out.println("a>=b " + (a>=b));//greater than or equal to //true
+		System.out.println("a<=b " + (a<=b));//less than or equal to //false
 
 	}
 
