6e5a51629cc4467f54cad73e8ba04be527ccb7ce
LPT-10 <LPT-10@10.0.0.110>
2024-11-23 14:29:55 +0530

    CommitParameterized

--- Svkp/src/org/tnsif/capgemini/c2tc/constructor/BeforeConstructor.java
+++ Svkp/src/org/tnsif/capgemini/c2tc/constructor/BeforeConstructor.java
@@ -1,19 +1,2 @@
 package org.tnsif.capgemini.c2tc.constructor;
 
-class Myclass
-{
-	String color;
-	int cost;
-	String Brand;
-}
-
-
-public class BeforeConstructor {
-
-	public static void main(String[] args) {
-		Myclass obj=new Myclass();
-		System.out.println(obj.color + " " +obj.cost + " "+obj.Brand);
-
-	}
-
-}
